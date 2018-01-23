99ccee26b650236f14af4bbfeed131c74d2dc43c
Chris Barber <christopher.l.barber@gmail.com>
2018-01-23 10:49:48 -0700

    updated migrations

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -8,16 +8,24 @@ contract Migrations {
     if (msg.sender == owner) _;
   }
 
-  function Migrations() {
-    owner = msg.sender;
-  }
+  function Migrations() 
+    public
+    {
+      owner = msg.sender;
+    }
 
-  function setCompleted(uint completed) restricted {
-    last_completed_migration = completed;
-  }
+  function setCompleted(uint completed)
+    public 
+    restricted 
+    {
+      last_completed_migration = completed;
+    }
 
-  function upgrade(address new_address) restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+  function upgrade(address new_address)
+    public 
+    restricted 
+    {
+      Migrations upgraded = Migrations(new_address);
+      upgraded.setCompleted(last_completed_migration);
+    }
 }
